828d52b021c461b1d8e1f92997068751f4777254
Andrey Dergach <thequasar7@gmail.com>
2021-04-25 20:59:00 +0300

    Implemented SignIn form

--- src/components/SignIn.svelte
+++ src/components/SignIn.svelte
@@ -1,1 +1,58 @@
-<h1>signin</h1>
+<script lang="ts">
+    import Textfield from '@smui/textfield/styled';
+    import HelperText from '@smui/textfield/helper-text/styled';
+    import Button, { Label } from '@smui/button/styled';
+    import Paper, { Title, Content } from '@smui/paper/styled';
+  
+    let username = '';
+    let email = '';
+    let password = '';
+    let confirm = '';
+  </script>
+  
+  <div class="glob-form">
+    <Paper color="back">
+      <Title>Вход</Title>
+      <Content class="glob-paper">
+        <div class="text">
+          <Textfield
+            style="width: 20em"
+            variant="standard"
+            bind:value={email}
+            label="Электронный адрес"
+            required
+          />
+        </div>
+  
+        <div class="text">
+          <Textfield
+            style="width: 20em"
+            variant="standard"
+            bind:value={password}
+            label="Пароль"
+            required
+          >
+            <HelperText slot="helper"
+              >Не менее 8 символов, должны быть заглавные буквы</HelperText
+            >
+          </Textfield>
+        </div>
+        <div class="btn">
+          <Button variant="raised">
+            <Label>Войти</Label>
+          </Button>
+        </div>
+      </Content>
+    </Paper>
+  </div>
+  
+  <style>
+    .text {
+      margin-top: 10px;
+    }
+  
+    .btn {
+      margin-top: 20px;
+    }
+  </style>
+  
