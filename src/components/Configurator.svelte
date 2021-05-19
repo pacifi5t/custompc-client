664371815a2b517f6cf6823449f185ace44692d7
Andrey Dergach <thequasar7@gmail.com>
2021-05-20 00:22:46 +0300

    Reworked configurator

--- src/components/Configurator.svelte
+++ src/components/Configurator.svelte
@@ -26,8 +26,8 @@
     }
   }
 
-  function findPartById(id: string) {
-    for (const part of parts) {
+  function findPartById(id: string, where: Array<any>) {
+    for (const part of allParts) {
       for (const elem of part.array) {
         if (id == elem.id) {
           return elem;
@@ -46,19 +46,25 @@
   }
 
   async function finishBuild() {
-    let sum = 0;
     let arr = [];
 
-    for (const elem of parts) {
-      const part = findPartById(selected[elem.type]);
+    for (const elem of partTypes) {
+      const part = findPartById(selectedParts[elem.type], partTypes);
       arr.push(part.id);
-      sum += part.price;
+    }
+
+    for (const elem of partArrayTypes) {
+      const array = selectedPartArrays[elem.type];
+      for (const subElem of array) {
+        const part = findPartById(subElem, partArrayTypes);
+        arr.push(part.id);
+      }
     }
 
     const result = await axios.post(requestUrl + '/custombuilds', {
       authorId: getCookie('uid'),
       name: name,
-      price: sum,
+      price: summaryPrice,
       warranty: 2,
       image: '',
       status: 'relevant',
@@ -67,113 +73,138 @@
     console.log(result);
   }
 
-  let name = '';
-  let parts = [
+  //For displaying
+  const partTypes = [
     new Part('Процессор', 'cpu'),
     new Part('Материнская плата', 'mb'),
     new Part('Оперативная память', 'ram'),
     new Part('Видеокарта', 'gpu'),
-    new Part('Накопители', 'storage'),
     new Part('Блок питания', 'psu'),
     new Part('Охлаждение', 'cooling'),
     new Part('Корпус', 'case')
   ];
-  let selected = {
+  const partArrayTypes = [new Part('Накопители', 'storage')];
+  const allParts = [...partTypes, ...partArrayTypes];
+
+  //Bound variables
+  let summaryPrice = 0;
+  let name = '';
+  let selectedParts = {
     cpu: '',
     mb: '',
     ram: '',
     gpu: '',
-    storage: [],
     psu: '',
     cooling: '',
     case: ''
   };
+  let selectedPartArrays = {
+    storage: []
+  };
 
-  for (let i = 0; i < parts.length; i++) {
-    getParts(parts[i].type)
+  //Init maps
+  for (let i = 0; i < partTypes.length; i++) {
+    getParts(partTypes[i].type)
       .then((val) => {
-        parts[i].array = val;
+        partTypes[i].array = val;
       })
       .catch((e) => console.error(e));
+  }
+  for (let i = 0; i < partArrayTypes.length; i++) {
+    getParts(partArrayTypes[i].type)
+      .then((val) => {
+        partArrayTypes[i].array = val;
+      })
+      .catch((e) => console.error(e));
+  }
+
+  //Reactive summary price update
+  $: {
+    summaryPrice = 0;
+    for (const elem of partTypes) {
+      const part = findPartById(selectedParts[elem.type], partTypes);
+      if (typeof part !== 'undefined') {
+        summaryPrice += part.price;
+      }
+    }
+
+    for (const elem of partArrayTypes) {
+      const array = selectedPartArrays[elem.type];
+      if (typeof array !== 'undefined') {
+        for (const subElem of array) {
+          const part = findPartById(subElem, partArrayTypes);
+          if (typeof part !== 'undefined') {
+            summaryPrice += part.price;
+          }
+        }
+      }
+    }
   }
 </script>
 
 <Paper style="margin-top: 20px;">
   <Title>Конфигуратор</Title>
   <Content>
-    <div style="width: 50%;">
-      <Textfield
-        style="width: 20em"
-        variant="standard"
-        bind:value={name}
-        label="Имя сборки"
-        required
-      >
-        <HelperText slot="helper">Назовите свое творение</HelperText>
-      </Textfield>
+    <div style="display: flex;">
+      <div class="half-page">
+        <Textfield
+          style="width: 20em"
+          variant="standard"
+          bind:value={name}
+          label="Имя сборки"
+          required
+        >
+          <HelperText slot="helper">Назовите свое творение</HelperText>
+        </Textfield>
 
-      <Group>
-        {#each parts as partType, i}
-          <Subheader style="margin-top: 40px;">{partType.label}</Subheader>
-          <List class="bordered">
-            {#each partType.array as part}
-              <Item class="bordered">
-                <Text>{part.name}</Text>
-                <Meta>
-                  <!-- <Checkbox bind:checked={selected[type.type]} value={part.name} /> -->
-                  <Radio
-                    color="primary"
-                    bind:group={selected[partType.type]}
-                    value={part.id}
-                  />
-                </Meta>
-              </Item>
-            {/each}
-          </List>
-        {/each}
-      </Group>
+        <Group>
+          {#each partTypes as partType}
+            <Subheader style="margin-top: 40px;">{partType.label}</Subheader>
+            <List class="bordered">
+              {#each partType.array as part}
+                <Item class="bordered">
+                  <Text>{part.name}</Text>
+                  <Meta>
+                    <Radio
+                      bind:group={selectedParts[partType.type]}
+                      value={part.id}
+                    />
+                  </Meta>
+                </Item>
+              {/each}
+            </List>
+          {/each}
 
-      <Button on:click={finishBuild}>Оформить заказ</Button>
+          {#each partArrayTypes as partType}
+            <Subheader style="margin-top: 40px;">{partType.label}</Subheader>
+            <List class="bordered">
+              {#each partType.array as part}
+                <Item class="bordered">
+                  <Text>{part.name}</Text>
+                  <Meta>
+                    <Checkbox
+                      bind:group={selectedPartArrays[partType.type]}
+                      value={part.id}
+                    />
+                  </Meta>
+                </Item>
+              {/each}
+            </List>
+          {/each}
+        </Group>
+
+        <Button on:click={finishBuild}>Оформить заказ</Button>
+      </div>
+      <div class="half-page">
+        <h2>Цена сборки: {summaryPrice} $</h2>
+      </div>
     </div>
   </Content>
 </Paper>
-<!-- 
-  <Subheader>Actors</Subheader>
-  <List>
-        <Item on:SMUI:action={() => (clicked = 'Bruce Willis')}>
-          <Text>Bruce Willis</Text>
-        </Item>
-        <Item on:SMUI:action={() => (clicked = 'Tom Hanks')}>
-          <Text>Tom Hanks</Text>
-        </Item>
-        <Item on:SMUI:action={() => (clicked = 'Jack Nicholson')}>
-          <Text>Jack Nicholson</Text>
-        </Item>
-        <Item on:SMUI:action={() => (clicked = 'Leonardo DiCaprio')}>
-          <Text>Leonardo DiCaprio</Text>
-        </Item>
-        <Item on:SMUI:action={() => (clicked = 'Matt Damon')}>
-          <Text>Matt Damon</Text>
-        </Item>
-      </List>
-      <Subheader>Books</Subheader>
-      <List>
-        <Item on:SMUI:action={() => (clicked = 'To Kill a Mockingbird')}>
-          <Text>To Kill a Mockingbird</Text>
-        </Item>
-        <Item on:SMUI:action={() => (clicked = 'The Great Gatsby')}>
-          <Text>The Great Gatsby</Text>
-        </Item>
-        <Item on:SMUI:action={() => (clicked = '1984')}>
-          <Text>1984</Text>
-        </Item>
-        <Item on:SMUI:action={() => (clicked = 'Catch-22')}>
-          <Text>Catch-22</Text>
-        </Item>
-        <Item
-          on:SMUI:action={() => (clicked = "Alice's Adventures in Wonderland")}
-        >
-          <Text>Alice's Adventures in Wonderland</Text>
-        </Item>
-      </List>
--->
+
+<style>
+  .half-page {
+    width: 50%;
+    margin: 20px;
+  }
+</style>
