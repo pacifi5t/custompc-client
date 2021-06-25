6f89a73adb0cec5da7cbd856682f8b266b4d18bd
Andrey Dergach <thequasar7@gmail.com>
2021-06-25 11:18:57 +0300

    Orders are now displayed

--- src/components/Cabinet.svelte
+++ src/components/Cabinet.svelte
@@ -9,7 +9,9 @@
   import List, { Item, Text, Group, Subheader, Meta } from '@smui/list/styled';
   import Select, { Option } from '@smui/select/styled';
   import Snackbar, { Actions } from '@smui/snackbar/styled';
-  import Paper, { Title, Content } from '@smui/paper/styled';
+  import Paper, { Title, Content, Subtitle } from '@smui/paper/styled';
+  import DataTable, { Head, Body, Row, Cell } from '@smui/data-table/styled';
+  import Card, { PrimaryAction, ActionButtons } from '@smui/card/styled';
   import { getCookie } from '../cookies';
   import { requestUrl } from '../utils';
   import axios from 'axios';
@@ -55,7 +57,7 @@
         });
         temp.push(b.data);
       }
-      orders.push(temp);
+      orders.push({ info: or, builds: temp });
     }
 
     orders = orders;
@@ -82,21 +84,53 @@
     {#await userData}
       <Text>...</Text>
     {:then res}
-      <Text>
+      <Subtitle>
         Дата регистрации - {new Date(res.data.created_at)
           .toDateString()
           .slice(4)}
-      </Text>
+      </Subtitle>
     {:catch err}
       <Text>{err}</Text>
     {/await}
-    <Title style="margin-top: 40px;">Список заказов</Title>
-    <div style="margin-top: 20px;">
-      {#each orders as o}
-        <p>build</p>
-      {/each}
-
-      
-    </div>
+    <Title style="margin-top: 60px;">Список заказов</Title>
+    {#each orders as ord}
+      <div class="order">
+        <Text>Заказ {ord.info.id}</Text>
+        <Text>Статус: {ord.info.status}</Text>
+        <Text>{new Date(ord.info.created_at).toDateString().slice(4)}</Text>
+      </div>
+      <DataTable
+        stickyHeader
+        table$aria-label="User list"
+        style="width: 100%; margin-bottom: 30px;"
+      >
+        <Head>
+          <Row>
+            <Cell numeic>№</Cell>
+            <Cell style="width: 100%;">Название сборки</Cell>
+            <Cell>Цена</Cell>
+            <Cell numeric>Гарантия</Cell>
+          </Row>
+        </Head>
+        <Body>
+          {#each ord.builds as build, i}
+            <Row>
+              <Cell numeric>{i + 1}</Cell>
+              <Cell style="width: 100%;">{build.name}</Cell>
+              <Cell>{build.price}</Cell>
+              <Cell numeric>{build.warranty}</Cell>
+            </Row>
+          {/each}
+        </Body>
+      </DataTable>
+    {/each}
   </Content>
 </Paper>
+
+<style>
+  .order {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+</style>
